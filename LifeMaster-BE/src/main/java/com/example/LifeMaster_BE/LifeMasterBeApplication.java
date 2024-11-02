d36385f0c0d5b3518172da508b7009f8574c184d
HOIVVVV <parkerpark@naver.com>
2024-11-02 20:01:54 +0900

    버전
    jdk 21
    java 21
    intelij로 작성

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/LifeMasterBeApplication.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/LifeMasterBeApplication.java
@@ -18,7 +18,7 @@ public class LifeMasterBeApplication {
 
 	@GetMapping("/hello")
 	public ResponseEntity<String> testApi() {
-		String result = "hello world!";
+		String result = "api 정상 실행";
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 }
