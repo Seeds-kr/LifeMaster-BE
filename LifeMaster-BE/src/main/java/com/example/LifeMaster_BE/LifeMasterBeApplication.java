f21464e840bd67946c3a5f30b7f8d578341d95ee
HOIVVVV <parkerpark@naver.com>
2024-11-02 20:00:36 +0900

    Initial commit

---
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/LifeMasterBeApplication.java
@@ -0,0 +1,24 @@
+package com.example.LifeMaster_BE;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  // Spring Boot 애플리케이션 설정
+@RestController
+public class LifeMasterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeMasterBeApplication.class, args);  // 애플리케이션 실행
+	}
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> testApi() {
+		String result = "hello world!";
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+}
