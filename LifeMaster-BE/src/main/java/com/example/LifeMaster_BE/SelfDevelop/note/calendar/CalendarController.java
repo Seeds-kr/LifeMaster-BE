41edb82e90fc7305d9f95b005c28f112ac72f9c3
solyrion <sde101573@gmail.com>
2024-11-16 15:21:30 +0900

    [Chore] 불필요한 코드 삭제

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/calendar/CalendarController.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/calendar/CalendarController.java
@@ -2,7 +2,6 @@ package com.example.LifeMaster_BE.SelfDevelop.note.calendar;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
