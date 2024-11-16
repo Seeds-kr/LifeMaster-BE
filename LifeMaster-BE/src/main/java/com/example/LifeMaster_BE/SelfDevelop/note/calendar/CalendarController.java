3b9493b7ed556a7cdb0360ca383b2cf5ef43f893
solyrion <sde101573@gmail.com>
2024-11-16 15:21:05 +0900

    [Feat] 자아성찰 달력 - 날짜별 정보(5감사, 일기) 반환

---
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/calendar/CalendarController.java
@@ -0,0 +1,28 @@
+package com.example.LifeMaster_BE.SelfDevelop.note.calendar;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestController
+@RequestMapping("/schedule/self-reflection")
+@RequiredArgsConstructor
+public class CalendarController {
+
+    private final CalendarService calendarService;
+
+    // ISO 8601 형식(yyyy-MM-dd 또는 yyyy-MM-dd'T'HH:mm:ss) 아닌 경우
+    // @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME
+    @GetMapping
+    public EventDto getEvent(@RequestParam("date") LocalDateTime date){
+        return calendarService.getEventsByDate(date);
+    }
+
+}
--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/diary/DiaryRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/diary/DiaryRepository.java
@@ -2,5 +2,10 @@ package com.example.LifeMaster_BE.SelfDevelop.note.diary;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Long> {
+
+    DiaryEntity findByDiaryDate(LocalDateTime diaryDate);
+
 }
