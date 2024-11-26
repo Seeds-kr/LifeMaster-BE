f90e0269a4b861e12e43b0fb383cea23ceb348ac
ChoiJiHooon <prin81208@naver.com>
2024-11-26 20:25:02 +0900

    feat: 수면 엔티티 생성

---
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Sleep/MoodStatus.java
@@ -0,0 +1,7 @@
+package com.example.LifeMaster_BE.TimeManager.Sleep;
+
+public enum MoodStatus {
+    Happy,    // 좋은 기분
+    BAD,     // 나쁜 기분
+    SOSO  // 보통 기분
+}
---
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Sleep/Sleep.java
@@ -0,0 +1,41 @@
+package com.example.LifeMaster_BE.TimeManager.Sleep;
+
+import com.example.LifeMaster_BE.UserManager.User;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sleep")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Sleep {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;  // 기본 키
+
+    @Column(nullable = false)
+    private LocalDateTime sleepDate;  // 수면 날짜
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;  // 사용자, User 엔티티와의 관계
+
+    @Column(nullable = false)
+    private LocalDateTime sleepStart;  // 수면 시작 시간
+
+    private LocalDateTime sleepEnd;  // 수면 종료 시간 (NULL 가능)
+
+    private LocalDateTime sleepAlarm; // 알람이 울린 시간 (NULL 가능)
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MoodStatus sleepMood;  // 오늘의 기분 (ENUM 타입)
+
+    @Column(nullable = true)
+    private Double sleepScore;  // 알람 점수
+
+}
