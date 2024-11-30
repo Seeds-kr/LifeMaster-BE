df1d8cc836a60eac610bae6cf394491dd2f2692a
solyrion <sde101573@gmail.com>
2024-11-30 16:42:19 +0900

    [feat] alarm, snooze 엔티티 생성

---
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/Snooze/SnoozeEntity.java
@@ -0,0 +1,28 @@
+package com.example.LifeMaster_BE.TimeManager.Alarm.Snooze;
+
+import com.example.LifeMaster_BE.TimeManager.Alarm.AlarmEntity;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+public class SnoozeEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    // Alarm
+    @OneToOne
+    @JoinColumn(name = "alarm_id")
+    private AlarmEntity alarm;
+
+    // 총 반복 횟수
+    private Long snoozeSum;
+    // 현재 반복 횟수
+    private Long snoozePre;
+    // 마지막 시간
+    private Timestamp snoozeLast;
+    // 시간 간격
+    private Timestamp snoozeInt;
+}
