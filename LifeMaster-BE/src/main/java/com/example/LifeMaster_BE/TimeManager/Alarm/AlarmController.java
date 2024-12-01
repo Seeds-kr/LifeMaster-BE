a128e316fccc3443a54cb86607a91c24013bfd62
solyrion <sde101573@gmail.com>
2024-12-01 16:10:16 +0900

    [feat] 알람 목록 기능 구현(알람 전체 리스트 반환, 특정 알람 반환, 특정 field on/off)

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmController.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmController.java
@@ -2,10 +2,32 @@ package com.example.LifeMaster_BE.TimeManager.Alarm;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController("/time/alarm")
 @RequiredArgsConstructor
 public class AlarmController {
+
+    private final AlarmService alarmService;
+
+    @GetMapping
+    public List<AlarmEntity> getAllAlarms(){
+        return alarmService.getAllAlarms();
+    }
+
+    @GetMapping("/{alarmId}")
+    public ResponseEntity<AlarmEntity> getAlarmById(@PathVariable Long alarmId){
+        AlarmEntity alarmById = alarmService.getAlarmById(alarmId);
+        return ResponseEntity.ok(alarmById);
+    }
+
+    @PostMapping("/{alarmId}/update-status")
+    public ResponseEntity<AlarmEntity> setAlarmStatus(@PathVariable Long alarmId, @RequestBody StatusDto statusDto){
+        AlarmEntity updatedAlarm = alarmService.updateBooleanField(alarmId, statusDto.getField(), statusDto.isStatus());
+        return ResponseEntity.ok(updatedAlarm);
+    }
 }
--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmService.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmService.java
@@ -1,12 +1,43 @@
 package com.example.LifeMaster_BE.TimeManager.Alarm;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AlarmService {
 
+    private final AlarmRepository alarmRepository;
+
+    public List<AlarmEntity> getAllAlarms(){
+        return alarmRepository.findAll();
+    }
+
+    public AlarmEntity getAlarmById(Long alarmId){
+        return alarmRepository.findById(alarmId)
+                .orElseThrow(() -> new EntityNotFoundException("Alarm" + alarmId + "not found"));
+    }
+
+    public AlarmEntity updateBooleanField(Long alarmId, String fieldName, boolean status){
+        AlarmEntity alarm = alarmRepository.findById(alarmId)
+                .orElseThrow(() -> new EntityNotFoundException("Alarm" + alarmId + "not found"));
+
+        try{
+            Field declaredField = AlarmEntity.class.getDeclaredField(fieldName);
+            declaredField.setAccessible(true);
+            declaredField.set(alarm, status);
+        }catch (NoSuchFieldException e){
+            throw new IllegalArgumentException("Field " + fieldName + " not found", e);
+        }catch (IllegalAccessException e){
+            throw new RuntimeException("Failed to update field: " + fieldName, e);
+        }
+
+        return alarmRepository.save(alarm);
+    }
 }
