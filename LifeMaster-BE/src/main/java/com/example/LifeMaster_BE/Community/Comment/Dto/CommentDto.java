59be84c0d824261b8fff30afdb76dcf49c559638
solyrion <sde101573@gmail.com>
2025-01-10 16:39:05 +0900

    [feature] dto 수정

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Comment/Dto/CommentDto.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Comment/Dto/CommentDto.java
@@ -2,9 +2,12 @@ package com.example.LifeMaster_BE.Community.Comment.Dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
+@Setter
+@NoArgsConstructor
 public class CommentDto {
 
     private String comment;
