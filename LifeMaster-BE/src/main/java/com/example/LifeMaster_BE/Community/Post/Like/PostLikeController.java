dfc3ef5ef5ddadc2ac349895a09d2e8e7dc1aa89
solyrion <sde101573@gmail.com>
2025-02-06 16:44:34 +0900

    [feat] Post, PostLike - CustomUserDetails 도입

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Post/Like/PostLikeController.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Post/Like/PostLikeController.java
@@ -1,5 +1,6 @@
 package com.example.LifeMaster_BE.Community.Post.Like;
 
+import com.example.LifeMaster_BE.Security.CustomUserDetails;
 import com.example.LifeMaster_BE.UserManager.Member.MemberEntity;
 import com.example.LifeMaster_BE.UserManager.Member.MemberRepository;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -22,19 +23,14 @@ import java.util.Map;
 public class PostLikeController {
 
     private final PostLikeService likeService;
-    private final MemberRepository memberRepository;
 
     @PostMapping("/{postId}")
     public ResponseEntity<Map<String, Boolean>> like (
             @Parameter(description = "게시글 ID", required = true)
             @PathVariable("postId") Long postId,
-            @AuthenticationPrincipal User user){
+            @AuthenticationPrincipal CustomUserDetails user){
 
-        String email = user.getUsername();
-        MemberEntity member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException(email));
-
-        boolean isLiked = likeService.toggleLike(member.getId(), postId);
+        boolean isLiked = likeService.toggleLike(user.getId(), postId);
         Map<String, Boolean> response = new HashMap<>();
         response.put("liked", isLiked);
 
--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Post/PostController.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Post/PostController.java
@@ -2,6 +2,7 @@ package com.example.LifeMaster_BE.Community.Post;
 
 import com.example.LifeMaster_BE.Community.Post.Dto.AllPostsDto;
 import com.example.LifeMaster_BE.Community.Post.Dto.PostDto;
+import com.example.LifeMaster_BE.Security.CustomUserDetails;
 import com.example.LifeMaster_BE.UserManager.Member.MemberEntity;
 import com.example.LifeMaster_BE.UserManager.Member.MemberRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,19 +25,17 @@ public class PostController {
 
     @Operation(summary = "게시글 전체 조회", description = "특정 유형(type)의 게시글 목록을 조회합니다.")
     @GetMapping
-    public ResponseEntity<List<AllPostsDto>> getAllPosts(@RequestParam("type") PostType type, @AuthenticationPrincipal User user) {
+    public ResponseEntity<List<AllPostsDto>> getAllPosts(@RequestParam("type") PostType type,
+                                                         @AuthenticationPrincipal CustomUserDetails user) {
 
-        String email = user.getUsername();
-        MemberEntity member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException(email));
-
-        List<AllPostsDto> allPosts = postService.getAllPosts(member.getId(), type);
+        List<AllPostsDto> allPosts = postService.getAllPosts(user.getId(), type);
         return ResponseEntity.ok(allPosts);
     }
 
     @Operation(summary = "게시글 작성", description = "새로운 게시글을 작성합니다.")
     @PostMapping
-    public ResponseEntity<PostEntity> newPost(@RequestBody PostDto postDto, @AuthenticationPrincipal User user) {
+    public ResponseEntity<PostEntity> newPost(@RequestBody PostDto postDto,
+                                              @AuthenticationPrincipal CustomUserDetails user) {
         String email = user.getUsername();
         String title = postDto.getTitle();
         String content = postDto.getContent();
@@ -63,15 +61,13 @@ public class PostController {
     @Operation(summary = "게시글 수정", description = "게시글 ID를 통해 특정 게시글을 수정합니다.")
     @PatchMapping("/{postId}")
     public void updatePost(@PathVariable Long postId,
-                           @RequestBody PostDto postDto, @AuthenticationPrincipal User user){
+                           @RequestBody PostDto postDto,
+                           @AuthenticationPrincipal CustomUserDetails user){
         String title = postDto.getTitle();
         String content = postDto.getContent();
         String fileUrl = postDto.getFile();
-        String email = user.getUsername();
-        MemberEntity member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException(email));
 
-        postService.updatePost(postId, title, content, fileUrl, member.getId());
+        postService.updatePost(postId, title, content, fileUrl, user.getId());
     }
 
     @Operation(summary = "게시글 삭제", description = "게시글 ID를 통해 특정 게시글을 삭제합니다.")
