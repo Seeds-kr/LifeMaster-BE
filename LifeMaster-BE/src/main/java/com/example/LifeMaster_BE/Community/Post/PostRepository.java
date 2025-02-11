0beedea7a85bf1b969a337c87afc3bcce7a29bd7
solyrion <sde101573@gmail.com>
2025-02-11 15:09:27 +0900

    [feat] 게시글 정렬(최신순)

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Post/PostRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Post/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.LifeMaster_BE.Community.Post;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
-    List<PostEntity> findByType(PostType type);
+    List<PostEntity> findByType(PostType type, Sort sort);
     Optional<PostEntity> findByIdAndMemberId(Long postId, Long memberId);
 
     @Modifying
