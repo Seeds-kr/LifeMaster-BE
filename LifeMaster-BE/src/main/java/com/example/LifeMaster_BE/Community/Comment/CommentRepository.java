e1f77de090db201ed31494eecf4b44e7331b59ea
solyrion <sde101573@gmail.com>
2025-05-30 16:23:03 +0900

    [feat] comment - post fetch join 적용 + 별도의 매서드로 분리

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Comment/CommentRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Comment/CommentRepository.java
@@ -1,5 +1,6 @@
 package com.example.LifeMaster_BE.Community.Comment;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,8 @@ public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
     void deleteByIdAndPostId(Long commentId, Long postId);
     Optional<CommentEntity> findByIdAndPostId(Long commentId, Long postId);
     List<CommentEntity> findByPostId(Long postId);
+
+    @EntityGraph(attributePaths = {"post"})
+    Optional<CommentEntity> findWithPostByIdAndPostId(Long commentId, Long postId);
+
 }
