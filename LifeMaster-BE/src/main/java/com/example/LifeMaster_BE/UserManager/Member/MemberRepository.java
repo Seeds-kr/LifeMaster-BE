7df31d84bb7664352d979761d6f98957e6710aad
solyrion <sde101573@gmail.com>
2025-06-25 17:17:48 +0900

    [refactor] 불필요한 코드 삭제

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/UserManager/Member/MemberRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/UserManager/Member/MemberRepository.java
@@ -1,7 +1,6 @@
 package com.example.LifeMaster_BE.UserManager.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Optional;
 
