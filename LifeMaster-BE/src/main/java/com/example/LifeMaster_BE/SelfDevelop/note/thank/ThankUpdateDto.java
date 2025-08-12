f3b744b14d576b9e31f267c277031fe1158b7dbf
solyrion <sde101573@gmail.com>
2025-08-12 15:27:28 +0900

    [refactor] 데이터 바인딩을 위한 기본생성자 추가(5감사)

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/thank/ThankUpdateDto.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/thank/ThankUpdateDto.java
@@ -1,9 +1,11 @@
 package com.example.LifeMaster_BE.SelfDevelop.note.thank;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ThankUpdateDto {
     private String thankOne;
