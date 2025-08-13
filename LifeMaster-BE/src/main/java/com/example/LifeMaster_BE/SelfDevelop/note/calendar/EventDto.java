a608e16be878d78f838cf830ecc1c3e7fedfb122
solyrion <sde101573@gmail.com>
2025-08-13 21:53:30 +0900

    [refactor] 데이터 바인딩을 위한 기본생성자 추가

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/calendar/EventDto.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/SelfDevelop/note/calendar/EventDto.java
@@ -1,9 +1,12 @@
 package com.example.LifeMaster_BE.SelfDevelop.note.calendar;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class EventDto {
 
