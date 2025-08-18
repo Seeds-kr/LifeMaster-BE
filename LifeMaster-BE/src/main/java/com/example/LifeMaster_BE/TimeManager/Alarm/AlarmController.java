93f0cd8b702ae33a395561b6a683b9275765bd50
solyrion <sde101573@gmail.com>
2025-08-18 20:21:44 +0900

    [refactor] 불필요한 주석 삭제

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmController.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmController.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
