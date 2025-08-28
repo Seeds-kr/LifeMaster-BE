ea4b0bca302a3465028db7ccda457815f742ca9b
Choi JiHun <159138577+prin81208@users.noreply.github.com>
2025-08-28 17:56:43 +0900

    SwaggerConfig 오타 수정
    SwaggerConfig 오타 수정

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Config/SwaggerConfig.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
                         .version("1.0")
                         .description("LifeMaster 백엔드 API 명세서"))
                 .servers(Arrays.asList(
-                        new Server().url("https://api.harvester.kr").description("Development Server"),
+                        new Server().url("https://api.lifemaster.harvester.kr").description("Development Server"),
                         new Server().url("http://localhost:7550").description("Local Server"),
                         new Server().url("http://localhost:8080").description("Local Server 2")
                 ))
