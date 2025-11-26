7cc45f0711d98ead63f711e27f83b9b40b642692
HOIVVVV <152933551+HOIVVVV@users.noreply.github.com>
2025-11-26 12:59:49 +0900

    UserVoteRepository Param 누락 추가

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Vote/UserVoteRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Community/Vote/UserVoteRepository.java
@@ -1,5 +1,6 @@
 package com.example.LifeMaster_BE.Community.Vote;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -7,6 +8,8 @@ import java.util.Optional;
 
 public interface UserVoteRepository extends JpaRepository<UserVote, Long> {
 
-    @Query("select uv.option.id from UserVote uv where uv.poll.id = :pollId and uv.member.id = :memberId")
-    Optional<Long> findMyOptionId(Long pollId, Long memberId);
+    @Query("select uv.option.id from UserVote uv " +
+            "where uv.poll.id = :pollId and uv.member.id = :memberId")
+    Optional<Long> findMyOptionId(@Param("pollId") Long pollId,
+                                  @Param("memberId") Long memberId);
 }
