0478c69e4bc476f62bc78cc12b64c574e0553068
Choi JiHun <159138577+prin81208@users.noreply.github.com>
2025-12-06 16:48:54 +0900

    Update SpringSecurityConfig.java
    - 인증 오류 관련 테스트

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Security/SpringSecurityConfig.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Security/SpringSecurityConfig.java
@@ -45,8 +45,8 @@ public class SpringSecurityConfig {
                                 //"/challenge/**",
                                 "/auth/password/reset/**",
                                 "/auth/kakao/**", "/kakao/**").permitAll()
-                        .requestMatchers("/private/**").authenticated()
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
+                        //.anyRequest().permitAll()
                 )
                 .exceptionHandling(e -> e
                         .authenticationEntryPoint((req, res, ex) -> {
