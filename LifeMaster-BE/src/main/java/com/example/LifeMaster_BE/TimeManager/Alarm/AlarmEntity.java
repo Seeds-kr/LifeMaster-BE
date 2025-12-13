dc030c2629ae522cd46f73ff041e265d17757c67
HOIVVVV <parkerpark@naver.com>
2025-12-13 19:35:01 +0900

    오류 수정

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmEntity.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/AlarmEntity.java
@@ -55,11 +55,11 @@ public class AlarmEntity {
     @Column(name = "is_snoozed", nullable = false)
     private boolean legacySnoozed;
 
-    private LocalDateTime snoozeTime;
+    private int snoozeTime;
     private Long snoozeCount;
 
-    private boolean reSlept;
-    private LocalDateTime reSleptTime;
+    private boolean reSleptPrevention;
+    private int reSleptPreventionTime;
     // === 기존 DB 컬럼 is_re_slept 호환용 더미 필드 ===
     @Column(name = "is_re_slept", nullable = false)
     private boolean legacyReSlept;  // 실제 로직에서는 안 쓰고, INSERT 시 값 채우기용
@@ -114,11 +114,11 @@ public class AlarmEntity {
         alarm.snoozeTime = dto.getSnoozeTime();
         alarm.snoozeCount = dto.getSnoozeCount();
 
-        alarm.reSlept = dto.isReSleptPrevention();
-        alarm.reSleptTime = dto.getReSleptPreventionTime();
+        alarm.reSleptPrevention = dto.isReSleptPrevention();
+        alarm.reSleptPreventionTime = dto.getReSleptPreventionTime();
 
         //더미
-        alarm.legacyReSlept = alarm.reSlept;
+        alarm.legacyReSlept = alarm.reSleptPrevention;
         alarm.legacySnoozed = alarm.snoozed;
 
         alarm.randomMissionType = dto.getRandomMissionType();
--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/Dto/ResponseAlarmDto.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/TimeManager/Alarm/Dto/ResponseAlarmDto.java
@@ -32,11 +32,11 @@ public class ResponseAlarmDto {
     private String alarmSound;
 
     private boolean snoozed;
-    private LocalDateTime snoozeTime;
+    private int snoozeTime;
     private Long snoozeCount;
 
     private boolean reSleptPrevention;
-    private LocalDateTime reSleptPreventionTime;
+    private int reSleptPreventionTime;
 
     // ==== 랜덤 미션 타입 ====
     @Schema(
