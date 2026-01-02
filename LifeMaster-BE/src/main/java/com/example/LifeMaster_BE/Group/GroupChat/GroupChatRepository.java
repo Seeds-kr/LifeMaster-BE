6ddca77f53ea4f163c9dd30130d96ab266a3153e
ChoiJiHooon <prin81208@naver.com>
2026-01-02 18:33:57 +0900

    feature: 그룹챗 과거 채팅기록 읽음 구현

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Group/GroupChat/GroupChatRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Group/GroupChat/GroupChatRepository.java
@@ -2,5 +2,8 @@ package com.example.LifeMaster_BE.Group.GroupChat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GroupChatRepository extends JpaRepository<GroupChatEntity, Long> {
+    List<GroupChatEntity> findByGroupIdOrderByTimestampAsc(Long groupId);
 }
