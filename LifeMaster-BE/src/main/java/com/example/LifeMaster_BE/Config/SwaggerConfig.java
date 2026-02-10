1e82378539e9bc326972ae2649ab0586a73d9cc5
HOIVVVV <parkerpark@naver.com>
2026-02-10 19:18:10 +0900

    로그 수정

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Config/SwaggerConfig.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Config/SwaggerConfig.java
@@ -27,7 +27,9 @@ public class SwaggerConfig implements WebMvcConfigurer {
                         .version("1.0")
                         .description("LifeMaster 백엔드 API 명세서"))
                 .servers(Arrays.asList(
-                        new Server().url("https://api.lifemaster.harvester.kr").description("Development Server"),
+                        new Server().url("http://ec2-54-180-88-104.ap-northeast-2.compute.amazonaws.com:8080").description("Development Server"),
+                        // 여기 주소만 변경
+                        //new Server().url("https://api.lifemaster.harvester.kr").description("Development Server"),
                         new Server().url("http://localhost:7550").description("Local Server"),
                         new Server().url("http://localhost:8080").description("Local Server 2")
                 ))
