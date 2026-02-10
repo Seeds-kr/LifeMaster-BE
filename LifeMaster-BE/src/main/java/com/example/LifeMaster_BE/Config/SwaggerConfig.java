69e6c05d3135f82113fd8040cdc44ad57be764fd
HOIVVVV <parkerpark@naver.com>
2026-02-10 20:29:04 +0900

    임시 서버 변경 사항 삭제

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Config/SwaggerConfig.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Config/SwaggerConfig.java
@@ -27,7 +27,8 @@ public class SwaggerConfig implements WebMvcConfigurer {
                         .version("1.0")
                         .description("LifeMaster 백엔드 API 명세서"))
                 .servers(Arrays.asList(
-                        new Server().url("http://ec2-54-180-88-104.ap-northeast-2.compute.amazonaws.com:7550").description("Development Server"),
+                        //new Server().url("http://ec2-54-180-88-104.ap-northeast-2.compute.amazonaws.com:8080").description("Development Server"),
+                        new Server().url("https://api.lifemaster.harvester.kr").description("Development Server"),
                         new Server().url("http://localhost:7550").description("Local Server"),
                         new Server().url("http://localhost:8080").description("Local Server 2")
                 ))
@@ -114,14 +115,6 @@ public class SwaggerConfig implements WebMvcConfigurer {
         return GroupedOpenApi.builder()
                 .group("Sleep")
                 .pathsToMatch("/sleep/**")
-                .build();
-    }
-
-    @Bean
-    public GroupedOpenApi GroupChatApi() {
-        return GroupedOpenApi.builder()
-                .group("GroupChat")
-                .pathsToMatch("/groups/**")
                 .build();
     }
 
