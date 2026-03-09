d9e30febb2b309606afa2070b5989b8b519ab0d3
ChoiJiHooon <prin81208@naver.com>
2026-03-09 22:56:54 +0900

    fix: On-premise 버전 통일

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Security/SpringSecurityConfig.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Security/SpringSecurityConfig.java
@@ -42,6 +42,7 @@ public class SpringSecurityConfig {
                         .requestMatchers(
                                 "/user/login",
                                 "/swagger-ui/**",
+                                "swagger-ui.html",
                                 "/v3/api-docs/**",
                                 "/user/register/**",
                                 "/auth/password/reset/**",
