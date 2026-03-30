1e6cc508cf43e268a92a449a1036da6015185c35
ChoiJiHooon <prin81208@naver.com>
2026-03-30 18:09:52 +0900

    fix: 직렬화 문제

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Challenge/ChallengeUserRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Challenge/ChallengeUserRepository.java
@@ -2,12 +2,15 @@ package com.example.LifeMaster_BE.Challenge;
 
 import com.example.LifeMaster_BE.UserManager.Member.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChallengeUserRepository extends JpaRepository<ChallengeUser, Long> {
-    List<ChallengeUser> findByUser(MemberEntity user);
+    @Query("SELECT cu FROM ChallengeUser cu JOIN FETCH cu.challenge WHERE cu.user = :user")
+    List<ChallengeUser> findByUser(@Param("user") MemberEntity user);
     Optional<ChallengeUser> findByUserAndChallenge(MemberEntity user, Challenge challenge);
     void deleteByChallenge(Challenge challenge);
     boolean existsByChallengeAndUser(Challenge challenge, MemberEntity user);
