0bccd091f9312bf2a1e115a89cc36f5e1df181a3
ChoiJiHooon <prin81208@naver.com>
2026-05-28 20:50:24 +0900

    feature: 디톡스반복잠금 api 추가 및 수정

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Challenge/Detox/RepeatDetoxDto.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Challenge/Detox/RepeatDetoxDto.java
@@ -1,42 +1,71 @@
 package com.example.LifeMaster_BE.Challenge.Detox;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+
 import java.util.List;
 
 public class RepeatDetoxDto {
 
     @Getter
     @Setter
+    @Schema(name = "RepeatDetoxCreateRequest")
     public static class Request {
 
         private String lockedApp;
-
         private Integer sessionUsageLimit;
-
         private Integer lockDuration;
-
         private Integer dailyMaxUsageLimit;
     }
 
     @Getter
     @Builder
+    @Schema(name = "RepeatDetoxResponse")
     public static class Response {
 
         private Long id;
-
         private String lockedApp;
-
         private Integer sessionUsageLimit;
-
         private Integer lockDuration;
-
         private Integer dailyMaxUsageLimit;
     }
 
     @Getter
     @Builder
+    @Schema(name = "RepeatDetoxListResponse")
     public static class ListResponse {
 
         private List<Response> lockedApps;
+    }
+
+    @Getter
+    @Builder
+    @Schema(name = "RepeatDetoxLockStatusResponse")
+    public static class LockStatusResponse {
+
+        private Long id;
+        private String lockedApp;
+        private boolean locked;
+    }
+
+    @Getter
+    @Builder
+    @Schema(name = "RepeatDetoxDetailResponse")
+    public static class DetailResponse {
+
+        private Long id;
+        private String lockedApp;
+
+        private Integer todayUsedMinutes;
+        private Integer remainingUnlockMinutes;
+        private boolean exceededDailyLimit;
+    }
+
+    @Getter
+    @Builder
+    @Schema(name = "RepeatDetoxPhraseResponse")
+    public static class PhraseResponse {
+
+        private String phrase;
     }
 }
