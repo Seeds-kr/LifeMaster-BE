cee396a3c965cda66b5f8b0f821de1d5145b327d
ChoiJiHooon <prin81208@naver.com>
2026-05-29 19:32:40 +0900

    feature: 디톡스반복잠금 오류 수정

--- LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Challenge/Detox/RepeatDetoxRepository.java
+++ LifeMaster-BE/src/main/java/com/example/LifeMaster_BE/Challenge/Detox/RepeatDetoxRepository.java
@@ -3,7 +3,9 @@ package com.example.LifeMaster_BE.Challenge.Detox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RepeatDetoxRepository extends JpaRepository<RepeatDetox, Long> {
-
+    List<RepeatDetox> findByMember_Id(Long userId);
 }
